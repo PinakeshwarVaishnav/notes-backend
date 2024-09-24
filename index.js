8b14e5e9d689738b816dfdf6a51f8ebfeafb61a9
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-24 16:25:56 +0530

    Deploying the database backend to production

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const generateId = () => {
 	return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 	const body = request.body
 
 	if (body.content === undefined) {
@@ -57,6 +57,7 @@ app.post('/api/notes', (request, response) => {
 	note.save().then(savedNote => {
 		response.json(savedNote)
 	})
+		.catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -83,14 +84,9 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-	const body = request.body
+	const { content, important } = request.body
 
-	const note = {
-		content: body.content,
-		important: body.important,
-	}
-
-	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+	Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
 		.then(updatedNote => {
 			response.json(updatedNote)
 		})
@@ -104,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformed id' })
+	} else if (error.name === 'Validation error') {
+		return response.status(400).json({ error: error.message })
 	}
 	next(error)
 }
