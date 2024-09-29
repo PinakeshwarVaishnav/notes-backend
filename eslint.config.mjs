934bbd7dcb808367705a8c3157af31d154430d19
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-09-29 17:11:53 +0530

    Update eslint and index.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ export default [
       '@stylistic/js': stylisticJs
     },
     rules: {
+      indent: ['error', 2],
       'eqeqeq': 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': [
